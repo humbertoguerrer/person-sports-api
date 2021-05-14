ba5a0995c4de89a42802acba2baf15a9df0f1aae
humbertoguerrer <humberto.guerrer@gmail.com>
2021-05-13 21:20:10 -0300

    Projeto criado

---
+++ src/main/java/com/hgn/personsportsapi/PersonSportsApiApplication.java
@@ -0,0 +1,13 @@
+package com.hgn.personsportsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonSportsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonSportsApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hgn/personsportsapi/PersonSportsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hgn.personsportsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonSportsApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
